7e3de11236cfda310c1fbdf1b0fa3b86ab683318
Tomasz <tomalek0@tlen.pl>
2018-11-08 18:43:48 +0100

    Stworzenie projektu

---
+++ src/main/java/com/ttuleja/bulletinboard/BulletinboardApplication.java
@@ -0,0 +1,12 @@
+package com.ttuleja.bulletinboard;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BulletinboardApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BulletinboardApplication.class, args);
+    }
+}
