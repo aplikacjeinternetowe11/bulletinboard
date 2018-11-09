6d6fbd5e4e337f95fdc408c614525f7333d8aec4
Tomasz <tomalek0@tlen.pl>
2018-11-09 21:01:44 +0100

    Dodano: - wyświetlanie buttonów w zależności od tego czy użytkownik jest gościem lub jest zalogowany - hashowanie hasła (w poprzednim commicie przez przypadek nie wysłano nowych plików) Funkcje dodane w poprzednim commicie: - możliwość logowania (SpringSecurity) - stronę testową na którą mogą wchodzić tylko zalogowani użytkownicy (Thymeleaf + SpringSecurity)

--- src/main/java/com/ttuleja/bulletinboard/Service/UserService.java
+++ src/main/java/com/ttuleja/bulletinboard/Service/UserService.java
@@ -2,6 +2,7 @@ package com.ttuleja.bulletinboard.Service;
 
 import com.ttuleja.bulletinboard.Dao.UserDao;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -10,6 +11,17 @@ public class UserService {
     private UserDao userDao;
 
     public void addUser(String username, String password, String phone_number) {
-        userDao.addUser(username,password,phone_number);
+        userDao.addUser(username,encode(password),phone_number);
+    }
+
+    public static String encode(String password){
+        int i = 0;
+        String hashedPassword = "";
+        while (i<10){
+            BCryptPasswordEncoder passwordEncoder = new BCryptPasswordEncoder();
+            hashedPassword = passwordEncoder.encode(password);
+            i++;
+        }
+        return hashedPassword;
     }
 }
